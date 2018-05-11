9038287679b158e5d65343bcfbc80d9571b7cabd
Pavel Anokhov <p.anokhov@gmail.com>
2018-05-11 12:39:58 +0300

    ANSDataContext target is Console application. Initial migration created.

--- ANSDataContext/DevicesContext.cs
+++ ANSDataContext/DevicesContext.cs
@@ -1,22 +1,32 @@
-﻿using Adamant.NotificationService.Models;
+﻿using System.IO;
+using Adamant.NotificationService.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Adamant.NotificationService.DataContext
 {
 	public class DevicesContext: DbContext
 	{
-		public static DevicesContext CreateContextWithSQLite(string connectionString)
-		{
-			var optionsBuilder = new DbContextOptionsBuilder<DevicesContext>();
-			optionsBuilder.UseSqlite(connectionString);
-
-			return new DevicesContext(optionsBuilder.Options);
-		}
-
 		public DevicesContext(DbContextOptions<DevicesContext> options) : base(options)
 		{
 		}
 
+		public DevicesContext() : base(OptionsWithConnectionString("appsettings.json"))
+		{
+		}
+
 		public DbSet<Device> Devices { get; set; }
+
+		private static DbContextOptions<DevicesContext> OptionsWithConnectionString(string json) {
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile(json, optional: false, reloadOnChange: true);
+			var configuration = builder.Build();
+
+			var connectionString = configuration.GetConnectionString("Devices");
+			var optionsBuilder = new DbContextOptionsBuilder<DevicesContext>();
+			optionsBuilder.UseSqlite(connectionString);
+			return optionsBuilder.Options;
+		}
 	}
 }
