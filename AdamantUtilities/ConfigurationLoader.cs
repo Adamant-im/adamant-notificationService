47f7b15315de8df35abb4b47bc7b6e10b477ecdd
Pavel Anokhov <p.anokhov@gmail.com>
2018-06-12 14:28:20 +0300

    AdamantUtilities project

---
+++ AdamantUtilities/ConfigurationLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Adamant
+{
+	public static class ConfigurationLoader
+	{
+		/// <summary>
+		/// Loads Configuration from '~/.ans'
+		/// </summary>
+		/// <returns>The default configuration.</returns>
+		public static IConfigurationRoot GetConfiguration()
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath("~/.ans/")
+				.AddJsonFile("config.json", false, true);
+
+			return builder.Build();
+		}
+	}
+}
